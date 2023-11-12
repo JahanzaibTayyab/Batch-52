34019412d19a95c98d1f675b90458de98c738c71
Jahanzaib Tayyab <jtayyab204@gmail.com>
2023-11-12 12:35:05 +0500

    12 Nov session batch52

---
+++ 12-Nov/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello i am ts file ok good one");
+let firstName = "Jahanzaib";
+let lastName = "Tayyab";
+console.log(firstName);
+console.log(lastName);
---
+++ 12-Nov/index.ts
@@ -0,0 +1,33 @@
+console.log("Hello i am ts file ok good one");
+
+let firstName = "Jahanzaib";
+const lastName = "Tayyab";
+
+console.log(firstName);
+
+console.log(lastName);
+
+//Variables Rules
+
+//1 can't contain any spaces
+//let name ggg='GHH'
+let space = 1234;
+
+// 2 variable can contain only numbers , letters , _ ,$
+
+let v1 = "23";
+let vvv = "2";
+let vv_2 = 45;
+let gg$ = 677;
+
+//let fg@=566
+//let fg!='rtt'
+
+// 3 variable name can't start with number
+
+//let 34hhh=56
+
+let g = 55;
+let _f = 55;
+let $g = 566;
+let f123 = "frgggg";
