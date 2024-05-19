5f22b483ee3f5212f25585956b7f2ce78cfc9f2c
Jahanziab Tayyab <jahanzaib.tayyab@mavrictech.com>
2024-05-19 09:41:40 +0500

    initial project start with pnpm

---
+++ learn-nextjs/husky-basics-react-life-cycle/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        t is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites still in their
+        infancy. Various versions have evolved over the years, sometimes by
+        accident, sometimes on purpose (injected humour and the like).
+      </div>
+    </main>
+  );
+}
